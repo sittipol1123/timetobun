4ea970b812dda5dda30cae262bb7f1eca2113106
stp.do <46347222+sittipol1123@users.noreply.github.com>
2024-10-11 19:46:27 +0700

    ...

--- controller/users/users.controller.ts
+++ controller/users/users.controller.ts
@@ -1,11 +1,67 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
 export class UserController {
   async index(data: any) {
     console.log(data);
     return { data };
   }
 
-  async store(data: any) {}
-  async find(id: string) {}
-  async update(data: { id: string; data: any }) {}
-  async delete(id: string) {}
+  async store(data: any) {
+    try {
+      await prisma.user.create({
+        data: {
+          username: data.username,
+          password: data.password,
+          fname: data.firstname,
+          lname: data.lastname,
+        },
+      });
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+  async find(id: string) {
+    try {
+      const response = await prisma.user.findFirst({
+        where: {
+          id: Number(id),
+        },
+      });
+      return response;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+  async update(id: string, data: any) {
+    try {
+      await prisma.user.update({
+        where: {
+          id: Number(id),
+        },
+        data: {
+          username: data.username,
+          updated_at: new Date(),
+        },
+      });
+      return true;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async delete(id: string) {
+    try {
+      await prisma.user.delete({
+        where: {
+          id: Number(id),
+        },
+      });
+      return true;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
--- controller/users/users.route.ts
+++ controller/users/users.route.ts
@@ -1,13 +1,30 @@
 import Elysia from "elysia";
 import { UserController } from "./users.controller";
+const users = new UserController();
 
 const Usersroute = new Elysia().group("/users", (app) =>
   app
     .get("/", () => {
       return { message: "this is a users page" };
     })
-    .post("/", ({ body }) => {
-      console.log(body);
+    .post("/", async ({ body }) => {
+      var result: boolean = await users.store(body);
+      var msg = { message: "error" };
+      if (result) {
+        msg = { message: "success" };
+      }
+      return msg;
+    })
+    .get("/:id", async ({ params: { id } }) => {
+      var result = await users.find(id);
+      if (result) {
+        return result;
+      }
+    })
+    .delete("/", async ({ params: { id } }) => {
+      if (await users.delete(id)) {
+        return { message: "success" };
+      }
     })
 );
 
